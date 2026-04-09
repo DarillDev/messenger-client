3999cc081187093075a150c77f419e77d38a5fee
Stepan Chekan <schekan@b2broker.com>
2026-04-10 02:20:54 +0800

    feat(pages/internal): add ChatItemComponent

---
+++ src/app/pages/internal/components/chat-item/chat-item.component.ts
@@ -0,0 +1,14 @@
+import { DatePipe, SlicePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { IChatDto } from '@shared/dtos/chat-dto.interface';
+
+@Component({
+  selector: 'app-chat-item',
+  imports: [RouterLink, RouterLinkActive, DatePipe, SlicePipe],
+  templateUrl: './chat-item.component.html',
+  styleUrl: './chat-item.component.scss',
+})
+export class ChatItemComponent {
+  public readonly chat = input.required<IChatDto>();
+}
