db78f61f6f8c95c7412caf78f5675cc176ea87f1
Stepan Chekan <schekan@b2broker.com>
2026-04-10 04:25:05 +0800

    feat(core/environment): add ApplicationEnvironment token and environment.json

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { APPLICATION_ENVIRONMENT } from '@core/environment/application-environment.token';
 
 import { App } from './app/app.component';
 import { appConfig } from './app/app.config';
+import environment from './environments/environment.json';
 
-bootstrapApplication(App, appConfig).catch(error => console.error(error));
+bootstrapApplication(App, {
+  providers: [
+    { provide: APPLICATION_ENVIRONMENT, useValue: environment },
+    ...appConfig.providers,
+  ],
+}).catch(error => console.error(error));
