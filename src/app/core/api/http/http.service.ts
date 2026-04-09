f865227769eae74f1d7d599e4776fec83c9d680f
Stepan Chekan <schekan@b2broker.com>
2026-04-10 04:27:37 +0800

    feat(core/api): add HttpService with environment-based URL

---
+++ src/app/core/api/http/http.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import {
+  APPLICATION_ENVIRONMENT,
+  IApplicationEnvironment,
+} from '@core/environment/application-environment.token';
+
+@Injectable({ providedIn: 'root' })
+export class HttpService {
+  private readonly http = inject(HttpClient);
+  private readonly env = inject<IApplicationEnvironment>(APPLICATION_ENVIRONMENT);
+
+  private url(path: string): string {
+    return `${this.env.apiUrl}${path}`;
+  }
+
+  public get<T>(path: string): Observable<T> {
+    return this.http.get<T>(this.url(path));
+  }
+
+  public post<T>(path: string, body: unknown): Observable<T> {
+    return this.http.post<T>(this.url(path), body);
+  }
+
+  public put<T>(path: string, body: unknown): Observable<T> {
+    return this.http.put<T>(this.url(path), body);
+  }
+
+  public delete<T>(path: string): Observable<T> {
+    return this.http.delete<T>(this.url(path));
+  }
+}
