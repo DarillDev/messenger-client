c4c0943b8a148d53a7dd37fe6e61db954c614725
Stepan Chekan <schekan@b2broker.com>
2026-04-10 02:21:01 +0800

    feat(pages/chat): add MessageBubbleComponent

---
+++ src/app/pages/chat/components/message-bubble/message-bubble.component.ts
@@ -0,0 +1,20 @@
+import { DatePipe } from '@angular/common';
+import { Component, computed, inject, input } from '@angular/core';
+import { IMessageDto } from '@shared/dtos/message-dto.interface';
+import { UserStore } from '@state/user/user.store';
+
+@Component({
+  selector: 'app-message-bubble',
+  imports: [DatePipe],
+  templateUrl: './message-bubble.component.html',
+  styleUrl: './message-bubble.component.scss',
+})
+export class MessageBubbleComponent {
+  private readonly userStore = inject(UserStore);
+
+  public readonly message = input.required<IMessageDto>();
+
+  protected readonly isSent = computed(
+    () => this.message().senderId === this.userStore.currentUser()?.userId,
+  );
+}
